e15bfd5e8fab7751205830dbd4eb00d9db076a00
Tony118g <guimaraesantony@gmail.com>
2023-03-10 17:56:38 +0000

    Add functionality for a user to change password

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from . import views
 from .forms import SignUpForm
+from django.urls import reverse_lazy
 from django.urls import path
 from allauth.account import views as auth_views
 
@@ -8,5 +9,6 @@ urlpatterns = [
     path('accounts/profile/', views.profile_page, name='profile_page'),
     path('accounts/edit_account/<str:pk>/', views.edit_account, name='edit_account'),
     path('accounts/delete_account/<str:pk>/', views.delete_account, name='delete_account'),
+    path('accounts/password/change/', auth_views.PasswordChangeView.as_view(success_url=reverse_lazy('profile_page'))),
     path('accounts/signup/', auth_views.SignupView.as_view(form_class=SignUpForm)),
 ]
