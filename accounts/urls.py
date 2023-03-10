a7e07977edbe2109b60e6eded8e335ec8886c75b
Tony118g <guimaraesantony@gmail.com>
2023-03-10 12:08:24 +0000

    Add functionality for user to edit account details

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,6 @@ from allauth.account import views as auth_views
 urlpatterns = [
     path('', views.home_page, name='home'),
     path('accounts/profile/', views.profile_page, name='profile_page'),
+    path('accounts/edit_account/<str:pk>/', views.EditAccount.as_view(), name='edit_account'),
     path('accounts/signup/', auth_views.SignupView.as_view(form_class=SignUpForm)),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.views.generic.edit import UpdateView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
 
 
 def home_page(request):
@@ -22,3 +26,20 @@ def profile_page(request):
         return render(request, 'profile.html', context)
     else:
         return redirect('home')
+
+
+class EditAccount(SuccessMessageMixin, UpdateView):
+    model = User
+    template_name = 'edit_account.html'
+    fields = [
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+            ]
+    success_url = reverse_lazy('profile_page')
+    success_message = 'Account successfully Updated'
+
+    def get_queryset(self):
+        query_set = User.objects.filter(id=self.request.user.id)
+        return query_set
