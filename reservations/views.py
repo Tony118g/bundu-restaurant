4a473c1b23506642d0f93714290f8065723f3efa
Tony118g <guimaraesantony@gmail.com>
2023-03-12 17:30:52 +0000

    Add functionality for a user to make a reservation

--- reservations/views.py
+++ reservations/views.py
@@ -1,10 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from reservations.models import Reservation
 from .forms import ReservationForm
 
 
 def make_reservation(request):
 
-    reservation_form = ReservationForm()
-    context = {'form': reservation_form}
+    if request.user.is_authenticated:
+        reservation_form = ReservationForm()
+        context = {
+            "form": reservation_form,
+        }
+    else:
+        return redirect('account_login')
+
+    if request.method == 'POST':
+
+        reservation_form = ReservationForm(request.POST)
+        if reservation_form.is_valid():
+            reservation_form.instance.user = request.user
+            reservation_form.instance.first_name = request.user.first_name
+            reservation_form.instance.last_name = request.user.last_name
+
+            reservation_form.save()
+            return redirect('home')
 
     return render(request, 'reserve.html', context)
