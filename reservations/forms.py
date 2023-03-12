8bc419e1ffda33768406421d69dceba709b35126
Tony118g <guimaraesantony@gmail.com>
2023-03-12 19:53:45 +0000

    Add validation for the date field for reservations
    Add form field attributes to provide a date picker with a minimum date

--- reservations/forms.py
+++ reservations/forms.py
@@ -1,8 +1,15 @@
 from django import forms
 from .models import Reservation
+from datetime import date, datetime, timedelta
+
+tomorrow = date.today() + timedelta(days=1)
 
 
 class ReservationForm(forms.ModelForm):
+
+    date = forms.DateField(widget=forms.TextInput(
+        attrs={'type': 'date', 'min': tomorrow}
+        ))
 
     class Meta:
         model = Reservation
