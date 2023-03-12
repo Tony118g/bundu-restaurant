79d3b8fcaa188be65e01635d3337227acbfee80b
Tony118g <guimaraesantony@gmail.com>
2023-03-12 10:36:09 +0000

    Make user name fields required and add max length

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,6 +6,14 @@ from django.contrib.auth.forms import UserCreationForm
 class SignUpForm(UserCreationForm):
 
     email = forms.EmailField(required=True)
+    first_name = forms.CharField(
+                                max_length=35,
+                                label='First name (up to 35 characters)'
+                                )
+    last_name = forms.CharField(
+                                max_length=35,
+                                label='Last name (up to 35 characters)'
+                                )
 
     class Meta(UserCreationForm.Meta):
         model = User
@@ -21,6 +29,14 @@ class SignUpForm(UserCreationForm):
 
 class EditUserForm(forms.ModelForm):
     email = forms.EmailField(required=True)
+    first_name = forms.CharField(
+                                max_length=35,
+                                label='First name (up to 35 characters)'
+                                )
+    last_name = forms.CharField(
+                                max_length=35,
+                                label='Last name (up to 35 characters)'
+                                )
 
     class Meta:
         model = User
