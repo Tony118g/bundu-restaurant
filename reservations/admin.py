1e346c35f6ab2a7a0794cac4f36c16d288be9ef4
Tony118g <guimaraesantony@gmail.com>
2023-03-12 14:15:04 +0000

    Add reservations to the admin panel

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Reservation
 
-# Register your models here.
+
+@admin.register(Reservation)
+class BookingAdmin(admin.ModelAdmin):
+    """
+    The class used to display reservations in admin panel
+    """
+    list_filter = (
+        'user',
+        'approved',
+        'denied',
+        'acknowledged',
+        'date'
+        )
+    list_display = (
+        'id',
+        'user',
+        'date',
+        'time',
+        'no_of_people',
+        'approved',
+        'denied',
+        'acknowledged'
+        )
