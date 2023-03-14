56458feb54835fa717c101b9e3152e025f5b0f4e
Tony118g <guimaraesantony@gmail.com>
2023-03-14 14:05:59 +0000

    Add functionality to display pending reservations
    Add a view to get pending reservations and render the template for staff
    Add a template to display the reservations in a table
    Link up the relevant url

--- staff/urls.py
+++ staff/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('staff/dashboard/', views.staff_dashboard, name='staff_dashboard'),
+    path('staff/pending/', views.pending_reservations, name='pending_reservations'),
 ]
--- staff/views.py
+++ staff/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from reservations.models import Reservation
 
 
 def staff_dashboard(request):
@@ -11,4 +12,18 @@ def staff_dashboard(request):
         context = {'user_count': user_count}
         return render(request, "dashboard.html", context)
     else:
+        return redirect('home')
+
+
+def pending_reservations(request):
+
+    if request.user.is_staff:
+        pending_list = Reservation.objects.filter(
+                                                acknowledged=False
+                                                ).order_by('date_of_request')
+        
+        context = {'pending_list': pending_list}
+        return render(request, 'pending_reservations.html', context)
+    else:
+        messages.warning(request, ("You are not authorized to view this page"))
         return redirect('home')
