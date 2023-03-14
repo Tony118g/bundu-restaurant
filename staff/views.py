1f2deabbc4c3ac2de35436b449bce1feb97f87ca
Tony118g <guimaraesantony@gmail.com>
2023-03-14 12:56:54 +0000

    Add content in dashboard to display customer count

--- staff/views.py
+++ staff/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 
 def staff_dashboard(request):
 
     if request.user.is_staff:
-        return render(request, "dashboard.html")
+        user_count = User.objects.filter(
+            is_superuser=False, is_staff=False
+            ).count()
+        context = {'user_count': user_count}
+        return render(request, "dashboard.html", context)
     else:
         return redirect('home')
