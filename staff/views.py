49c1a9db3f5df3e83adc3a35eaa053a91e2a2075
Tony118g <guimaraesantony@gmail.com>
2023-03-15 15:09:45 +0000

    Add missing import for render_to_string

--- staff/views.py
+++ staff/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.template.loader import render_to_string
 from django.contrib.auth.models import User
 from django.conf import settings
 from reservations.models import Reservation
