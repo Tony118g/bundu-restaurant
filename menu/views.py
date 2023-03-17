990d71322d7e734fda6fc30fa4680eb7e36f82de
Tony118g <guimaraesantony@gmail.com>
2023-03-17 14:02:11 +0000

    Add feeback messages for adding menu items

--- menu/views.py
+++ menu/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import MenuItem
 from .forms import MenuItemForm
 
@@ -11,9 +12,23 @@ def add_menu_item(request):
             form = MenuItemForm(request.POST, request.FILES)
             if form.is_valid():
                 form.save()
+                if form.instance.status == 1:
+                    messages.success(
+                        request,
+                        ("Menu item successfully published.")
+                        )
+                else:
+                    messages.success(
+                        request,
+                        ("Menu draft saved as draft.")
+                        )
                 return redirect('staff_dashboard')
         else:
             context = {'form': form}
             return render(request, "add_item.html", context)
     else:
+        messages.warning(
+            request,
+            ("Only logged in staff members can view this page")
+            )
         return redirect('home')
