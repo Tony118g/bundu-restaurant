2a26a86e0401962defb746ec88f8872580f04f94
Tony118g <guimaraesantony@gmail.com>
2023-03-17 13:16:36 +0000

    Add code for menu images to upload to cloudinary

--- menu/views.py
+++ menu/views.py
@@ -5,15 +5,15 @@ from .forms import MenuItemForm
 
 def add_menu_item(request):
 
-    form = MenuItemForm()
     if request.user.is_staff:
+        form = MenuItemForm()
         if request.method == 'POST':
-            form = MenuItemForm(request.POST)
+            form = MenuItemForm(request.POST, request.FILES)
             if form.is_valid():
                 form.save()
                 return redirect('staff_dashboard')
+        else:
+            context = {'form': form}
+            return render(request, "add_item.html", context)
     else:
         return redirect('home')
-
-    context = {'form': form}
-    return render(request, "add_item.html", context)
