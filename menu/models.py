e60983d1b82acbac77cb20bbe72a7a7d155f0d76
Tony118g <guimaraesantony@gmail.com>
2023-03-17 11:51:40 +0000

    Add a model for menu items to the menu app

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+MENU_CATEGORIES = (
+    ('starter', 'starter'),
+    ('main', 'main'),
+    ('desert', 'desert'),
+    )
+
+
+class MenuItem(models.Model):
+
+    title = models.CharField(max_length=200, unique=True)
+    featured_image = CloudinaryField('image', default='placeholder')
+    description = models.TextField(default='Menu item description')
+    available = models.BooleanField(default=True)
+    price = models.DecimalField(
+        null=True, max_digits=4, decimal_places=2, default='00.00'
+        )
+    category = models.CharField(
+        choices=MENU_CATEGORIES, default='main', max_length=7
+        )
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    def __str__(self):
+        return self.title
