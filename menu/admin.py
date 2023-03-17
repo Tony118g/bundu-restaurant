a9180bd04cc598a0eb5163d43c0eb40a2ebad815
Tony118g <guimaraesantony@gmail.com>
2023-03-17 12:28:35 +0000

    Add a section for the menu to the admin panel

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import MenuItem
 
-# Register your models here.
+
+@admin.register(MenuItem)
+class MenuAdmin(admin.ModelAdmin):
+    """
+    The class used to display menu items in the admin panel
+    """
+    list_filter = (
+        'category',
+        'available',
+        'status'
+        )
+    list_display = (
+        'title',
+        'category',
+        'available',
+        'status'
+        )
