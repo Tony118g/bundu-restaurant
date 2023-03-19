ff6951ff9d02875957bd955c25e4947390ffd4e5
Tony118g <guimaraesantony@gmail.com>
2023-03-19 13:57:48 +0000

    Add functionality to display menu item drafts
    Add a view and template to render a page with menu item drafts
    Link up the relevant url

--- menu/urls.py
+++ menu/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('menu/', views.menu_page, name='menu_page'),
+    path('menu/drafts/', views.menu_drafts, name='menu_drafts'),
     path('menu/add_menu_item/', views.add_menu_item, name='add_menu_item'),
 ]
--- menu/views.py
+++ menu/views.py
@@ -38,9 +38,9 @@ def menu_page(request):
     """
     Renders the menu page
     """
-    starter_items = MenuItem.objects.filter(status=0, category='starter')
-    main_items = MenuItem.objects.filter(status=0, category='main')
-    desert_items = MenuItem.objects.filter(status=0, category='desert')
+    starter_items = MenuItem.objects.filter(status=1, category='starter')
+    main_items = MenuItem.objects.filter(status=1, category='main')
+    desert_items = MenuItem.objects.filter(status=1, category='desert')
 
     context = {
         'starter_items': starter_items,
@@ -48,3 +48,24 @@ def menu_page(request):
         'desert_items': desert_items,
         }
     return render(request, 'menu.html', context)
+
+
+def menu_drafts(request):
+    """
+    Renders the menu drafts page
+    """
+
+    if request.user.is_staff:
+        menu_drafts = MenuItem.objects.filter(status=0)
+        context = {
+            'menu_drafts': menu_drafts,
+        }
+
+        return render(request, 'menu_drafts.html', context)
+
+    else:
+        messages.warning(
+            request,
+            ("Only logged in staff members can view this page")
+            )
+        return redirect('home')
